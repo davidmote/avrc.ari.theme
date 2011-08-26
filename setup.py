e285d603018335735d2b9ec299a20b58e790564b
Marco Martinez <usblackbird@gmail.com>
2011-08-26 18:51:33 +0000

    Cleanup.

--- setup.py
+++ setup.py
@@ -3,20 +3,18 @@ import os
 
 version = '0.6.0'
 
-setup(name='avrc.ari.theme',
+setup(
+    name='avrc.ari.theme',
     version=version,
     description="AIDS Research Institute XDV Theme",
-    long_description=open("README.txt").read() + "\n" +
-                     open(os.path.join("docs", "HISTORY.txt")).read(),
-    # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-      "Framework :: Plone",
-      "Programming Language :: Python",
-      ],
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
     keywords='',
-    author='',
-    author_email='',
-    url='http://svn.plone.org/svn/collective/',
+    author='BEAST Core Development Team',
+    author_email='beast@ucsd.edu',
+    url='http://datam0nk3y.org/',
     license='GPL',
     packages=find_packages('src', exclude=['ez_setup']),
     package_dir={'':'src'},
@@ -26,11 +24,8 @@ setup(name='avrc.ari.theme',
     install_requires=[
         'setuptools',
         'plone.app.theming',
-        # -*- Extra requirements: -*-
-    ],
+        ],
     entry_points="""
-    # -*- Entry points: -*-
-
     [z3c.autoinclude.plugin]
     target = plone
     """,
